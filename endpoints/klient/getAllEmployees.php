3eb054aaf825fbb6d5cbd96161f1d65a06a1c197
ZEEF1337 <exclaim0@gmail.com>
2020-06-12 09:49:54 +0200

    Nyt endpoint til at hente alle brugere med.

---
+++ endpoints/klient/getAllEmployees.php
@@ -0,0 +1,68 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once ($_SERVER['DOCUMENT_ROOT']."/SvendAPI/functions/propFunctions.php");
+
+
+if(!isset($_GET['token'])){
+    $out['result'] = 0;
+    $out['message'] = "Mangler parameter";
+    $json = json_encode($out);
+    print_r($json);
+    return;
+}
+
+$givenToken = $_GET['token'];
+
+$supporterToken = CheckSupporterToken($givenToken);
+if($supporterToken['Result'] != 1){
+    $out['result'] = 0;
+    $out['message'] = "Kun for personale";
+    $json = json_encode($out);
+    print_r($json);
+    return;
+}
+
+$database = new Database();
+$db = $database->getConnection();
+
+$query = "SELECT users.ID, users.Fornavn, users.Efternavn, ur.Navn AS Rolle, us.Navn AS Specialitet FROM users";
+$query .= " INNER JOIN user_roles AS ur ON ur.ID = users.Rolle";
+$query .= " INNER JOIN user_specialties AS us ON us.ID = users.Specialitet ORDER BY users.ID DESC;";
+
+$stmt = $db->prepare($query);
+
+try{
+    $stmt->execute();
+    $num = $stmt->rowCount();
+    $out = array();
+    $out["records"] = array();
+    if($num > 0){
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $temp = array(
+                "ID" => (int)$ID,
+                "Navn" => "$Fornavn $Efternavn",
+                "Rolle" => $Rolle,
+                "Specialitet" => $Specialitet
+            );
+            array_push($out["records"], $temp);
+        }
+        $out['result'] = 1;
+    }else{
+        $out['result'] = 0;
+        $out['message'] = "Ingen resultater fundet";
+    }
+} catch(PDOException $e){
+    $out['result'] = 0;
+    $out['message'] = $e->getMessage();
+}
+$json = json_encode($out);
+print_r($json);
+return;
+?>
